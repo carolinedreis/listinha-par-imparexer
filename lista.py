a6664938d1ad3d5b6998e49752ee31637ca8b0fa
Caroline Reis <caroline.reissp@gmail.com>
2019-12-11 15:46:31 -0300

    lista-pares-impar

---
+++ lista.py
@@ -0,0 +1,9 @@
+lista= [[], []]
+for u in range (1,6):
+numero=int(input('Digite um numero:  '))
+if numero / 2 ==0:
+núm[0].append(numero)
+else:
+núm[1].append(numero)
+print ('-= *)
+print(f' 'todos os numeros: {núm})
